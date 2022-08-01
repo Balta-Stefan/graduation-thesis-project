1cf82efa822de81e3bb7e6707948d20a1ce62764
Balta-Stefan <pivad@protonmail.ch>
2022-08-01 13:03:46 +0200

    App - performed refactoring and removed commented blocks of code.App is now using S3 compatible object storage for checkpointing.

--- Simple energy aggregator/src/main/java/simpleaggregator/App.java
+++ Simple energy aggregator/src/main/java/simpleaggregator/App.java
@@ -20,29 +20,44 @@ public class App
     private static final int countryWindowDurationSeconds = 3;
     private static final int totalByCityWindowDurationSeconds = 3;
 
-    private static final String checkpointLocation = "C:\\Users\\Korisnik\\Desktop\\Spark checkpoint\\";
+    private static final String checkpointRootLocation = "s3a://simple-energy-aggregator/aggregator-checkpoints/"; // "C:\\Users\\Korisnik\\Desktop\\Spark checkpoint\\";
+    private static final String cityAggregationsCheckpointLocation = checkpointRootLocation + "city-aggregations-checkpoints";
+    private static final String countryAggregationsCheckpointLocation = checkpointRootLocation + "country-aggregations-checkpoints";
+    private static final String hourlyConsumerAggregationsCheckpointLocation = checkpointRootLocation + "hourlyConsumer-aggregations-checkpoints";
+
     private static final String kafkaBootstrapServers = "127.0.0.1:9092";
     private static final String inputTopicName = "input";
 
-    public static void main(String[] args) throws TimeoutException, StreamingQueryException
+    public static void main(String[] args) throws TimeoutException, StreamingQueryException, AnalysisException
     {
         // https://spark.apache.org/docs/latest/submitting-applications.html#master-urls
         // https://databricks.com/blog/2017/04/26/processing-data-in-apache-kafka-with-structured-streaming-in-apache-spark-2-2.html
+        // https://stackoverflow.com/questions/37132559/add-jar-files-to-a-spark-job-spark-submit
 
-        // C:/Users/Korisnik/Desktop/spark-3.3.0-bin-hadoop3/bin/spark-submit.cmd --master local --deploy-mode client --class simpleaggregator.App --name Testiranje --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 "D:\Diplomski rad\Prakticni dio\Simple energy aggregator\target\simple-energy-aggregator-1.0-SNAPSHOT.jar"
+        // spark-submit.cmd --master local --deploy-mode client --class simpleaggregator.App --name Testiranje --packages org.apache.spark:spark-sql-kafka-0-10_2.13:3.3.0,org.apache.spark:spark-hadoop-cloud_2.13:3.3.0 "D:\Diplomski rad\Prakticni dio\Simple energy aggregator\target\simple-energy-aggregator-1.0-SNAPSHOT.jar"
 
         // setup
-
         SparkSession spark = SparkSession
                 .builder()
                 .appName("Simple energy aggregator Spark application")
                 .config("spark.scheduler.mode", "FAIR")
-                .config("spark.sql.shuffle.partitions", 5)
-                .config("spark.sql.streaming.checkpointLocation", checkpointLocation)
+                .config("spark.sql.shuffle.partitions", 1)
+                .config("spark.sql.streaming.checkpointLocation", checkpointRootLocation)
                 .config("spark.sql.session.timeZone", "UTC")
-                //.master("local")
+                .config("spark.hadoop.fs.s3a.endpoint", "localhost:9000")
+                .config("spark.hadoop.fs.s3a.access.key", "vQStvk5ileb3Mhw0")
+                .config("spark.hadoop.fs.s3a.secret.key", "4rhALdLpPa8IBXxDehI69Ki3613krErB")
+                .config("spark.hadoop.fs.s3a.committer.name", "directory")
+                .config("spark.sql.sources.commitProtocolClass", "org.apache.spark.internal.io.cloud.PathOutputCommitProtocol")
+                .config("spark.hadoop.fs.s3a.path.style.access", true)
+                //.config("spark.hadoop.fs.s3a.connection.timeout", 10000)
+                .config("spark.hadoop.fs.s3a.connection.ssl.enabled", false)
+                .master("local")
                 .getOrCreate();
         spark.sparkContext().setLogLevel("ERROR");
+
+        // the purpose of spark.hadoop.fs.s3a.path.style.access is explained at https://medium.com/@e.osama85/the-difference-between-the-amazon-s3-path-style-urls-and-virtual-hosted-style-urls-4fafd5eca4db
+        // and https://www.redhat.com/en/blog/anatomy-s3a-filesystem-client
 
         StructType messageSchema =  new StructType()
                 .add("meterID", DataTypes.LongType, false)
@@ -53,26 +68,18 @@ public class App
 
         //messageSchema.printTreeString();
 
-        Dataset<Row> rowData = spark
+        Dataset<Row> inputData = spark
                 .readStream()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", kafkaBootstrapServers)
                 .option("subscribe", inputTopicName)
-                //.option("startingOffsets", "earliest")
                 .load()
                 .select(from_json(col("value").cast(DataTypes.StringType), messageSchema).alias("parsed_value"))
                 .select("parsed_value.*")
-                .withColumn("parsed_timestamp", from_unixtime(col("timestamp")).cast(DataTypes.TimestampType));
-                /*.format("json")
-                .option("header", false)
-                .option("multiline", true)
-                .schema(messageSchema)
-                .json("file:///C:\\Users\\Korisnik\\Desktop\\spark input files\\")
-                .withColumn("parsed_timestamp", expr("timestamp").cast(DataTypes.TimestampType));*/
-        //rowData.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)");
+                .withColumn("timestamp", from_unixtime(col("timestamp")).cast(DataTypes.TimestampType));
 
-        rowData.printSchema();
-        rowData
+        inputData.printSchema();
+        inputData
                 .writeStream()
                 .format("console")
                 .outputMode(OutputMode.Append())
@@ -81,12 +88,27 @@ public class App
 
         // aggregations
 
+        /*
+        // alternative way in mostly-SQL
+        inputData
+                .withWatermark("timestamp", "30 minutes")
+                .createTempView("inputTable");
+        Dataset<Row> hourlySumByConsumer = inputData
+                .sqlContext()
+                .sql("SELECT meterID, " +
+                        " named_struct('start', to_unix_timestamp(window.start), 'end', to_unix_timestamp(window.end)) as unix_window," +
+                        " SUM(activeDelta) as aggregatedActiveDelta," +
+                        " SUM(reactiveDelta) as aggregatedReactiveDelta" +
+                        " FROM inputTable" +
+                        " GROUP BY meterID, window(timestamp, '1 hour')");
+        */
+
         // consumer consumptions will be aggregated hourly to minimize disk usage on kafka cluster (with lower retention policy)
-        Dataset<Row> hourlySumByConsumer = rowData
-                .withWatermark("parsed_timestamp", "30 minutes")
+        Dataset<Row> hourlySumByConsumer = inputData
+                .withWatermark("timestamp", "30 minutes")
                 .groupBy(
                         col("meterID"),
-                        window(col("parsed_timestamp"), "1 hour")
+                        window(col("timestamp"), "1 hour")
                 )
                 .sum("activeDelta", "reactiveDelta")
                 .withColumnRenamed("sum(activeDelta)", "aggregatedActiveDelta")
@@ -100,11 +122,11 @@ public class App
 
 
         final String totalConsumptionByCityWindowDuration = totalByCityWindowDurationSeconds + " seconds";
-        Dataset<Row> totalByCity = rowData
-                .withWatermark("parsed_timestamp", "5 seconds")
+        Dataset<Row> totalByCity = inputData
+                .withWatermark("timestamp", "5 seconds")
                 .groupBy(
                         col("cityID"),
-                        window(col("parsed_timestamp"), totalConsumptionByCityWindowDuration)
+                        window(col("timestamp"), totalConsumptionByCityWindowDuration)
                 )
                 .sum("activeDelta", "reactiveDelta")
                 .withColumnRenamed("sum(activeDelta)", "aggregatedActiveDelta")
@@ -117,10 +139,10 @@ public class App
         totalByCity.printSchema();
 
         final String totalConsumptionWindowDuration = countryWindowDurationSeconds + " seconds";
-        Dataset<Row> totalConsumption = rowData
-                .withWatermark("parsed_timestamp", "5 seconds")
+        Dataset<Row> totalConsumption = inputData
+                .withWatermark("timestamp", "5 seconds")
                 .groupBy(
-                        window(col("parsed_timestamp"), totalConsumptionWindowDuration)
+                        window(col("timestamp"), totalConsumptionWindowDuration)
                 )
                 .sum("activeDelta", "reactiveDelta")
                 .withColumnRenamed("sum(activeDelta)", "aggregatedActiveDelta")
@@ -136,6 +158,7 @@ public class App
                 .writeStream()
                 .format("console")
                 .option("truncate", false)
+                //.option("spark.sql.streaming.checkpointLocation", hourlyConsumerAggregationsCheckpointLocation)
                 .outputMode(OutputMode.Update())
                 .start();
 
@@ -143,6 +166,7 @@ public class App
                 .writeStream()
                 .format("console")
                 .option("truncate", false)
+                //.option("spark.sql.streaming.checkpointLocation", cityAggregationsCheckpointLocation)
                 .outputMode(OutputMode.Update())
                 .start();
 
@@ -150,12 +174,11 @@ public class App
                 .writeStream()
                 .format("console")
                 .option("truncate", false)
-                .outputMode(OutputMode.Update())
+                .option("spark.sql.streaming.checkpointLocation",  countryAggregationsCheckpointLocation)
+                //.outputMode(OutputMode.Update())
                 .start();
 
         // TODO detect anomalies in user reporting (readers shouldn't report a lower total value than in the last report)
-
-        //totalByCity.printSchema();
 
         StreamingQuery hourlyByConsumerQuery = hourlySumByConsumer
                 .select(to_json(struct("*")).alias("value"))
@@ -165,6 +188,7 @@ public class App
                 //.option("truncate", false)
                 .option("kafka.bootstrap.servers", kafkaBootstrapServers)
                 .option("topic", hourlyConsumerTopic)
+                .option("spark.sql.streaming.checkpointLocation", hourlyConsumerAggregationsCheckpointLocation)
                 .start();
         StreamingQuery totalByCityQuery = totalByCity
                 .select(to_json(struct("*")).alias("value"))
@@ -174,6 +198,7 @@ public class App
                 //.option("truncate", false)
                 .option("kafka.bootstrap.servers", kafkaBootstrapServers)
                 .option("topic", totalByCityTopic)
+                .option("spark.sql.streaming.checkpointLocation", cityAggregationsCheckpointLocation)
                 .start();
         StreamingQuery totalConsumptionQuery = totalConsumption
                 .select(to_json(struct("*")).alias("value"))
@@ -183,13 +208,9 @@ public class App
                 //.option("truncate", false)
                 .option("kafka.bootstrap.servers", kafkaBootstrapServers)
                 .option("topic", totalConsumptionTopic)
+                .option("spark.sql.streaming.checkpointLocation", countryAggregationsCheckpointLocation)
                 .start();
 
         spark.streams().awaitAnyTermination();
-        /*query1.awaitTermination();
-        System.out.println("======================= behind query1.awaitTermination");
-        query2.awaitTermination();
-        System.out.println("======================= behind query2.awaitTermination");
-        query3.awaitTermination();*/
     }
 }
