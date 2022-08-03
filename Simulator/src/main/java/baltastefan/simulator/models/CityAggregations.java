f6e6d4bb6c59d624cde8ae9dcfa78b785b32cb93
Balta-Stefan <pivad@protonmail.ch>
2022-08-03 14:18:37 +0200

    Modified CityAggregations class;

--- Simulator/src/main/java/baltastefan/simulator/models/CityAggregations.java
+++ Simulator/src/main/java/baltastefan/simulator/models/CityAggregations.java
@@ -6,14 +6,17 @@ import lombok.ToString;
 
 @NoArgsConstructor
 @ToString(callSuper = true)
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = true, exclude = {"latitude", "longitude"})
 public class CityAggregations extends Aggregation
 {
     public long cityID;
+    public double latitude, longitude;
 
-    public CityAggregations(double activeDelta, double reactiveDelta, long cityID, Window window)
+    public CityAggregations(double latitude, double longitude, double activeDelta, double reactiveDelta, long cityID, Window window)
     {
         super(activeDelta, reactiveDelta, window);
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.cityID = cityID;
     }
 }
