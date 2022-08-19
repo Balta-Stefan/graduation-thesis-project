94b8975f66c7b97922ae2027b846e4a7b1ed8d0a
Balta-Stefan <pivad@protonmail.ch>
2022-08-19 15:39:37 +0200

    Spark App class - renamed CounterMessage to MeterReading.

--- simple-energy-aggregator/src/main/java/simpleaggregator/models/MeterReading.java
+++ simple-energy-aggregator/src/main/java/simpleaggregator/models/MeterReading.java
@@ -3,14 +3,14 @@ package simpleaggregator.models;
 import java.io.Serializable;
 import java.time.Instant;
 
-public class CounterMessage implements Serializable
+public class MeterReading implements Serializable
 {
     public final long meterID;
     public final long cityID;
     public final Instant timestamp;
     public final double activeDelta;
 
-    public CounterMessage(long meterID, long cityID, Instant timestamp, double activeDelta)
+    public MeterReading(long meterID, long cityID, Instant timestamp, double activeDelta)
     {
         this.meterID = meterID;
         this.cityID = cityID;
