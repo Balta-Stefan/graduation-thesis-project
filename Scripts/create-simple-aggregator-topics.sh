7a8c21cee250b6a2bc29ae9e250390d1761c5123
Balta-Stefan <pivad@protonmail.ch>
2022-08-19 12:45:43 +0200

    Edited the script for creating topics - added retention policies.

--- Scripts/create-simple-aggregator-topics.sh
+++ Scripts/create-simple-aggregator-topics.sh
@@ -1,6 +1,27 @@
 #! /bin/bash
 
-/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 --topic hourlyConsumptionByConsumer --create --partitions 3
-/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 --topic totalByCity --create --partitions 3
-/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 --topic totalConsumption --create --partitions 3
-/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 --topic input --create --partitions 3
+/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 \
+			--topic hourlyConsumptionByConsumer \
+			--create \
+			--partitions 3 \
+			--config retention.ms=108000000 \
+			--config cleanup.policy=compact,delete \
+			--config max.compaction.lag.ms=1200000
+			
+/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 \
+			--topic totalByCity \
+			--create \
+			--partitions 3 \
+			--config retention.ms=600000
+			
+/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 \
+			--topic totalConsumption \
+			--create \
+			--partitions 3 \
+			--config retention.ms=600000
+			
+/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 \
+			--topic input \
+			--create \
+			--partitions 3 \
+			--config retention.ms=600000
