ba74f5e3f6ed0c6af41432f3f9dfcc5d67ee7d1a
Balta-Stefan <pivad@protonmail.ch>
2022-09-07 14:29:48 +0200

    Updated Kafka init script

--- Scripts/create-simple-aggregator-topics.sh
+++ Scripts/create-simple-aggregator-topics.sh
@@ -6,7 +6,7 @@
 			--partitions 3 \
 			--config retention.ms=108000000 \
 			--config cleanup.policy=compact,delete \
-			--config max.compaction.lag.ms=1200000
+			--config max.compaction.lag.ms=600000
 			
 /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 \
 			--topic totalByCity \
@@ -17,7 +17,7 @@
 /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 \
 			--topic totalConsumption \
 			--create \
-			--partitions 3 \
+			--partitions 1 \
 			--config retention.ms=600000
 			
 /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server desktop-kafka-1:9092 \
